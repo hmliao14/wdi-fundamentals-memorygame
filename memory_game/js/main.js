2a3706d736a5915d373a2aa95561bce30179c7c6
hmliao14 <hmliao14@gmail.com>
2017-07-27 13:50:31 -0700

    Created game logic for 10.6 asg
    Also added in additional changes myself to make the game more ideal and easier to debug:
    - added a reset function resetFlip() to pop out the cardsInPlay array so it will check for new match after player flipped two cards.
    - added an empty log message " " after checkForMatch() to easily debug output in console.
    P.S. Still need to check for suit match and implement click event to display images clicked.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,63 @@
 
-var cards = ["queen","queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
+      console.log(" ");
   } else {
       console.log("Sorry, try again.");
+      console.log(" ");
   }
+};
+
+var resetFlip = function() {
+	for (var i = cardsInPlay.length - 1; i >= 0; i--) {
+		cardsInPlay.pop();
+	}
 };
 
 var flipCard = function(cardId){
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank 
+		+ " of " + cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
+		resetFlip();
 	}
 };
 
+
 flipCard(0);
 flipCard(1);
+
+flipCard(0);
+flipCard(2);
+
+flipCard(2);
+flipCard(3);
