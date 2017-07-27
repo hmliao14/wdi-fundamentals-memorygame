f9c294a030e69e637719e771ca31850e72587531
hmliao14 <hmliao14@gmail.com>
2017-07-26 21:43:36 -0700

    Created game logic for 8.8 unit assignment

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen","queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var choseTwo = false;
+var match = false;
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+	choseTwo = true;
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		match = true;
+		console.log("Found a Match!");
+	} else {
+		match = false;
+		console.log("Sorry, Please Try Again"); 
+	}
+} else choseTwo = false;
+
+console.log(cardsInPlay);
