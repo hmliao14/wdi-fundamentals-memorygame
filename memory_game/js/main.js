9bb90648b16b521b974893ca5046e7d7d4548a7e
hmliao14 <hmliao14@gmail.com>
2017-07-28 00:01:18 -0700

    Created game logic for 11.6 before bonus
    At this stage, all required assignments listed on the pre-work for this memory game have been completed. Hence this commit will be my deliverable until I have cracked out the following bonus challenge and extra features I have in mind:
    
    Goal for bonus/extras:
    - Track user scores
    - Bigger board/Better aesthetic
    - Implement randomness/difficulties (match suit as well)
    - Fade in game board/reset button
    - Maybe more!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,39 +25,60 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+	/* added additional console log message and an 
+	 * empty log message for easy debug purpose. 
+	 * Alert requires acknowledgment before proceeding.
+	 */ 
+    if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
+      alert("You found a match!");
       console.log(" ");
-  } else {
+
+    } else {
       console.log("Sorry, try again.");
+      alert("Sorry, try again.");
       console.log(" ");
-  }
+    }
 };
 
+/* This method somewhat ensure that the pair of cards 
+ * flipped, or the two cards that we are compairing is 
+ * always new. Else, flipping something like queen/king,
+ * then king/queen, will result 2nd time to
+ * be a match because king/king in 2nd flip and 3rd flip.
+*/ 
 var resetFlip = function() {
 	for (var i = cardsInPlay.length - 1; i >= 0; i--) {
 		cardsInPlay.pop();
 	}
 };
 
-var flipCard = function(cardId){
-
+var flipCard = function(){
+	// Additional log message on console for easy debug purposes
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank 
 		+ " of " + cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 		resetFlip();
 	}
 };
 
+var createBoard = function(argument) {
+	// Create a loc variable makes easier to read
+	// and easier to reference later.
+	var loc = document.getElementById('game-board');
+	var newElement;
+	for (var i =0; i <cards.length; i++) {
+		newElement = document.createElement('img');
+		newElement.setAttribute('src',"images/back.png");
+		newElement.setAttribute('data-id', i);
+		newElement.addEventListener('click', flipCard)
+		loc.appendChild(newElement);
+	}
+}
 
-flipCard(0);
-flipCard(1);
-
-flipCard(0);
-flipCard(2);
-
-flipCard(2);
-flipCard(3);
+createBoard();
